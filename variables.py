ac05ee037dd659e83fc71d0d925c3793bda0b368
officialjwise <officialjwise20@gmail.com>
2024-01-23 11:31:35 +0000

    Added some variable samples with f-strings

--- variables.py
+++ variables.py
@@ -1,8 +1,15 @@
-x = 5
-y = 10
+gladys = "Gladys"
+phil = "My name is Phil"
+daniel = "My name is Daniel"
 
-z = x + y
-print(z)
+isBoy = True
+isGirl = False
 
-for i in range(1, 10):
-  print(i)
+age1 = 10
+age2 = 20
+print(f"The sum of the ages is {age1 + age2}")
+
+print(f"My name is {gladys}")
+
+print(f"My name is {phil}")
+print(f"{isBoy} {isGirl}")
